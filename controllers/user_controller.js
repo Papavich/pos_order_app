2064058708fac1c20f6375587b23cbb04785e84b
Papavich Pasri <33253796+Papavich@users.noreply.github.com>
2024-04-04 13:33:12 +0700

    Update user_controller.js
    check approve before login
    and get current data after update approve

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,42 +18,47 @@ var userController = {
             // เช็คในบานข้อมูลมี email หรือไม่
             let userInstance = await User.findOne({userEmail:userEmail})
             if(userInstance){
-                // console.log("is user ", userInstance);
-                // ถ้ามีอีเมลก็ให้ทำการ เช็ค password ต่อไป
-                let decodePassword = await bcrypt.compare(userPassword,userInstance.userPassword);
-                console.log("decode password = ", decodePassword);
-                
-                if(decodePassword) {
 
-                    // console.table(userInstance.userEmail, userInstance.userName);
-                    console.log(userInstance.userEmail);
-                    console.log(userInstance.userName);
+                // check approve
+                if(userInstance.userApprove){
+                    // console.log("is user ", userInstance);
+                    // ถ้ามีอีเมลก็ให้ทำการ เช็ค password ต่อไป
+                    let decodePassword = await bcrypt.compare(userPassword,userInstance.userPassword);
+                    console.log("decode password = ", decodePassword);
+                    if(decodePassword) {
+                        // console.table(userInstance.userEmail, userInstance.userName);
+                        console.log(userInstance.userEmail);
+                        console.log(userInstance.userName);
 
-                    // create token for user
-                    let token = await jwt.sign({userEmail:userInstance.userEmail, userName:userInstance.userName, userRole:userInstance.userRole}, SECRET, {algorithm: 'HS256'});
-                    console.log("token = ", token);
+                        // create token for user
+                        let token = await jwt.sign({userEmail:userInstance.userEmail, userName:userInstance.userName, userRole:userInstance.userRole}, SECRET, {algorithm: 'HS256'});
+                        console.log("token = ", token);
 
-                    // ถ้า password ถูก ให้ return ดังนี้
-                    return res.status(200).json({
-                    message:"Login Sucessful",
-                    data: token
-                    
-                });
+                        // ถ้า password ถูก ให้ return ดังนี้
+                        return res.status(200).json({
+                        message:"Login Sucessful",
+                        data: token
+                        });
+                    } else {
+                        // ถ้า password ผิด ให้ return ดังนี้
+                        return res.status(400).json({
+                            status:400,
+                            message:"Login Fails Wrong Email or Password" 
+                            });
+                        }
                 } else {
-                    // ถ้า password ผิด ให้ return ดังนี้
-                   return res.status(400).json({
-                    status:400,
-                    message:"Login Fails Wrong Email or Password" 
-                    });
+                        // return login fails
+                        res.status(401).json({
+                            status: 401,
+                            message: "Login Fails User Not Approve!"
+                        })
                 }
-
                 
-
             } else {
                 return  res.status(400).json({
                             status:400,
                             message:"Login Fails Wrong Email or Password"
-                        })}
+            })}
             
         } catch (error) {   
             res.status(500).send(error.toString());
@@ -66,13 +71,17 @@ var userController = {
             // const id = "1234"
             // console.log("id user = ", id);
             // เรียกใช้งาน model เพื่อทำการ update userApprove
-            const userUpdateInstacne = await User.findByIdAndUpdate(id, {userApprove: true})
+            const userUpdateInstacne = await User.findByIdAndUpdate(id, {userApprove: true});
+
+            //เช็คว่ามันมี user นี้จริง ๆ ไหม
             if(userUpdateInstacne){
+                // ดึงข้อมูลตัวปัจจับันดัวย
+                const userCurrentData = await User.findById(id);
                 console.log("update success");
                 return  res.status(201).json({
                         message:"User Approved Successfully!",
-                        data: userUpdateInstacne
-                })
+                        data: userCurrentData
+                });
             } else {
                 console.log("update fails");
                 return  res.status(400).json({
